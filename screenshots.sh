fbccf97e2e41b5fcb091ad68ab12a31ee64c76d6
Rob Moss <robm.dev@gmail.com>
2025-10-01 13:04:32 +1000

    Use a white background for screenshots

--- screenshots.sh
+++ screenshots.sh
@@ -3,5 +3,5 @@
 # Save the title page and final slide as PNG images.
 #
 
-convert -density 300 example.pdf[0] -resize 25% titlepage.png
-convert -density 300 example.pdf[5] -resize 25% slide.png
+convert -density 300 example.pdf[0] -background white -flatten -resize 25% titlepage.png
+convert -density 300 example.pdf[5] -background white -flatten -resize 25% slide.png
